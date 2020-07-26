a17d03ce00c26eec1b48d071e4f2cc55d30592c4
LinZiYu1996 <2669093302@qq.com>
2020-07-26 08:43:51 +0800

    7-26

--- sql_50/src/main/java/com/lin/sql_50/mapper/StudentMapper.java
+++ sql_50/src/main/java/com/lin/sql_50/mapper/StudentMapper.java
@@ -1,7 +1,9 @@
 package com.lin.sql_50.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lin.sql_50.entity.Score;
 import com.lin.sql_50.entity.Student;
+import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.List;
@@ -14,13 +16,56 @@ import java.util.List;
  * @author Lin
  * @since 2020-07-24
  */
+
+@Repository
 public interface StudentMapper extends BaseMapper<Student> {
 
 
-
-
+    /**
+     * 查询男生、女生人数
+     * @return
+     */
     List<HashMap<String, Integer>> countManAndWoManNumber();
 
+
+    /**
+     * 查询每门课程被选修的学生数
+     * @return
+     */
     List<HashMap<Integer, Integer>> countCoursesByStudents();
+
+    /**
+     *统计每门课程的学生选修人数（超过 num人的课程才统计
+     */
+    List<HashMap<Integer, Integer>> countStudentsMaxtoNum(Integer num);
+
+    /**
+     * 检索至少选修 num 门 课程的学生学号
+     * @param num
+     * @return
+     */
+    List<HashMap<Integer, Integer>> countCoursesMaxtoNum(Integer num);
+
+
+    /**
+     * 查询平均成绩大于等于 score 的所有学生的学号、姓名和平均成绩
+     * @param score
+     * @return
+     */
+    List<HashMap<Integer, Student>> averageScore(Integer score);
+
+    /**
+     * 查询不及格的课程及学生名，学号，按课程号从大到小排列】
+     * @param score
+     * @return
+     */
+//    List<HashMap<Student, Score>> getInfoStudentUnder60(Integer score);
+
+
+    /**
+     * 查询所有同学的学生编号、学生姓名、选课总数、所有课程的总成绩（没成绩的显示为 null）
+     * @return
+     */
+    List<HashMap<String, String>> getAll();
 
 }
--- sql_50/src/test/java/com/lin/sql_50/Sql50ApplicationTests.java
+++ sql_50/src/test/java/com/lin/sql_50/Sql50ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.lin.sql_50;
 
+import com.lin.sql_50.entity.Score;
+import com.lin.sql_50.entity.Student;
 import com.lin.sql_50.mapper.StudentMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -32,7 +34,6 @@ class Sql50ApplicationTests {
     }
 
 
-
     @Test
     public void t2() {
 
@@ -45,7 +46,67 @@ class Sql50ApplicationTests {
     }
 
 
+    @Test
+    public void t3() {
 
+        List<HashMap<Integer, Integer>> hashMaps = studentMapper.countStudentsMaxtoNum(5);
+        log.info(hashMaps.toString());
+//[{选修人数=6, cid=01}, {选修人数=6, cid=02}, {选修人数=6, cid=03}]
+
+
+    }
+
+
+    @Test
+    public void t4() {
+
+        List<HashMap<Integer, Integer>> hashMaps = studentMapper.countCoursesMaxtoNum(3);
+        log.info(hashMaps.toString());
+
+
+//[{sid=01}, {sid=02}, {sid=03}, {sid=04}]
+
+
+    }
+
+    @Test
+    public void t5() {
+
+
+        List<HashMap<Integer, Student>> averageScores = studentMapper.averageScore(85);
+        log.info(averageScores.toString());
+
+        //[{avg(cscore)=89.66667, sname=赵雷, sid=01}, {avg(cscore)=93.50000, sname=郑竹, sid=07}]
+
+
+    }
+
+//
+//    @Test
+//    public void t6() {
+//
+//        List<HashMap<Student, Score>> getInfoStudentUnder60 = studentMapper.getInfoStudentUnder60(60);
+//    }
+
+
+    @Test
+    public void t7() {
+
+        List<HashMap<String, String>> all = studentMapper.getAll();
+
+        log.info(all.toString());
+
+//        [{总成绩=269.0, 选课数=3, sname=赵雷, sid=01},
+//        {总成绩=210.0, 选课数=3, sname=钱电, sid=02},
+//        {总成绩=240.0, 选课数=3, sname=孙风, sid=03},
+//        {总成绩=100.0, 选课数=3, sname=李云, sid=04}
+//        , {总成绩=163.0, 选课数=2, sname=周梅, sid=05},
+//        {总成绩=65.0, 选课数=2, sname=吴兰, sid=06},
+//        {总成绩=187.0, 选课数=2, sname=郑竹, sid=07},
+//        {选课数=0, sname=王菊, sid=08}]
+
+
+    }
 
 
 }
